58ccb1b9cd8655f8708ef312ec2375014b633274
Adrien Simon <adrien.simon@etu.utc.fr>
2022-06-15 00:03:48 +0200

    restructured project

--- python/testing.py
+++ python/testing.py
@@ -4,7 +4,7 @@ def main():
     print("SIMULATION TOUS LES NIVEAUX")
     for i in range(1,10):
         print("------------------------------------")
-        os.system(f"python3 main.py -l ./levels/level{i}.txt")
+        os.system(f"python3 main.py -l ../levels/level{i}.txt")
     pass
 
 if __name__ == "__main__":
