e8d8d610d2b680d0d055f854b665b38e0035616c
Leo Peron <leo.peron@etu.utc.fr>
2022-06-14 10:48:59 +0200

    All levels works

--- main.py
+++ main.py
@@ -51,9 +51,11 @@ def insert_tail(s, l):
     l.append(s)
     return l
 
-def insert_head(s ,l):
+
+def insert_head(s, l):
     l.insert(0, s)
     return s
+
 
 def remove_head(l):
     return l.pop(0), l
@@ -166,11 +168,12 @@ def updating_state(
     blocks: List[Tuple[int, int]] = [],
     keyFinded: bool = False,
     lockOpenable: bool = False,
+    mobCleared: bool = False
 ):
 
     # KILLINGS MOBS
     newMobs = list(state.mobs)
-    if mobs:
+    if mobs or mobCleared:
         newMobs = list(mobs)
     for mob in newMobs:
         if (mob in state.safeTraps) or (mob in map_rules["S"]):
@@ -219,9 +222,6 @@ def do_inplace(action: Action, state: State, map_rules: Dict[str, set]):
     hero = state.hero
     blocks = state.blocks
     mobs = state.mobs
-    spikes = map_rules["S"]
-    safeTraps = state.safeTraps
-    unsafeTraps = state.unsafeTraps
     key = state.key
     lock = state.lock
 
@@ -231,7 +231,12 @@ def do_inplace(action: Action, state: State, map_rules: Dict[str, set]):
     free = free_factory(map_rules)
 
     if action.verb == "move":
-        if free(newHero) and not (newHero in mobs | blocks) and newHero != lock and newHero != key:
+        if (
+            free(newHero)
+            and not (newHero in mobs | blocks)
+            and newHero != lock
+            and newHero != key
+        ):
             return updating_state(map_rules, state, newHero)
 
     if action.verb == "push":
@@ -259,10 +264,17 @@ def do_inplace(action: Action, state: State, map_rules: Dict[str, set]):
 
     if action.verb == "kill":
         newPose = one_step(newHero, action.direction)
-        if (newHero in mobs) and (not free(newPose) or newPose in blocks or newPose == lock):
-            return updating_state(
-                map_rules, state, hero, mobs=moving_frozenset(mobs, newHero)
-            )
+        if (newHero in mobs) and (
+            not free(newPose) or newPose in blocks or newPose == lock
+        ):
+            if not moving_frozenset(mobs, newHero):
+                return updating_state(
+                    map_rules, state, hero, mobs=moving_frozenset(mobs, newHero), mobCleared=True
+                )
+            else:
+                return updating_state(
+                    map_rules, state, hero, mobs=moving_frozenset(mobs, newHero)
+                )
 
     if action.verb == "unlock/key":
 
@@ -303,17 +315,17 @@ def succ_factory(map_rules: Dict[str, set]) -> Set[Tuple[State, Action]]:
 
     return succ
 
+
 # DEFINING HEURISTIC
-def manhattan_distance_factory(map_rules: Dict[str,set]) -> Callable:
-    def dist_to_closest_demon(position: Tuple[int,int])->int:
-        goals = map_rules['D']
-        firstGoal = list(goals)[0]
-        minPose = abs(firstGoal[0] - position[0]) + abs(firstGoal[1] - position[1])
-        for goal in goals:
-            if abs(goal[0] - position[0]) + abs(goal[1] - position[1]) < minPose:
-                minPose = abs(goal[0] - position[0]) + abs(goal[1] - position[1])
-        return minPose
+def manhattan_distance_factory(map_rules: Dict[str, set]) -> Callable:
+    def dist_to_closest_demon(position: Tuple[int, int]) -> int:
+        return min(
+            abs(position[0] - demon[0]) + abs(position[1] - demon[1])
+            for demon in map_rules["D"]
+        )
+
     return dist_to_closest_demon
+
 
 # SEARCH ALGO (GREEDY)
 def search_greedy(s0, goals, succ, remove, insert, heuristic, debug=True):
@@ -344,10 +356,11 @@ def search_greedy(s0, goals, succ, remove, insert, heuristic, debug=True):
         insert(saveState, l)
     return None, save
 
+
 # SEARCH ALGO (A*)
 
-# SEARCH ALGO (DSF)
-def search_dsf(s0, goals, succ, remove, insert, debug=True):
+# SEARCH ALGO (DSF & BSF)
+def search_with_parents(s0: State, goals: Callable, succ: Callable, remove: Callable, insert: Callable, debug: bool=True) -> Tuple[State, Dict[State, Action]]:
     l = [s0]
     save = {s0: None}
     s = s0
@@ -366,28 +379,6 @@ def search_dsf(s0, goals, succ, remove, insert, debug=True):
                     return s2, save
                 insert(s2, l)
     return None, save
-
-# SEARCH ALGO (BSF)
-def search_bsf(s0, goals, succ, remove, insert, debug=True):
-    l = [s0]
-    save = {s0: None}
-    s = s0
-    while l:
-        s, l = remove(l)
-        for s2, a in succ(s).items():
-            if not s2:
-                continue
-            if debug:
-                print("Previous State=", s)
-                print("A=", a)
-                print("New State =", s2)
-            if not s2 in save:
-                save[s2] = (s, a)
-                if goals(s2):
-                    return s2, save
-                insert(s2, l)
-    return None, save
-
 
 def main():
     # récupération du nom du fichier depuis la ligne de commande
@@ -399,7 +390,7 @@ def main():
         infos["grid"], infos["max_steps"], infos["m"], infos["n"]
     )
 
-    end, save = search_bsf(
+    end, save = search_with_parents(
         s0,
         goal_factory(map_rules),
         succ_factory(map_rules),
@@ -408,7 +399,7 @@ def main():
         debug=False,
     )
 
-    # end, save = search_dsf(
+    # end, save = search_with_parents(
     #     s0,
     #     goal_factory(map_rules),
     #     succ_factory(map_rules),
@@ -427,8 +418,6 @@ def main():
     #     debug=True,
     # )
 
-
-    # calcul du plan
     if end:
         plan = "".join([a for _, a in dict2path(end, save) if a])
         if check_plan(plan):
@@ -439,16 +428,6 @@ def main():
         print("Pas de solution trouvée")
 
     sys.exit(2)
-
-def testing():
-    mobs = set()
-    mobs.add((3, 4))
-    mobs.add((1, 7))
-    mobs.add((2, 9))
-    frozen = frozenset(mobs)
-    print(moving_frozenset(frozen, (3, 4), (7, 6)))
-    pass
-
 
 if __name__ == "__main__":
     main()
